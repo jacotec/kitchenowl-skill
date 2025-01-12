60e35f02c9de7b885757c8aea19b60e162b4c506
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2025-01-12 13:41:01 +0000

    initial import, basic kitchenowl wrapper

---
+++ README.md
@@ -0,0 +1,1 @@
+Alexa Skill to manage KitchenOwl lists.
---
+++ lambda/backends/kitchenowl.py
@@ -0,0 +1,63 @@
+import os
+import requests
+import logging
+from functools import cached_property
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class KitchenOwlAPI:
+    def __init__(self, household_id):
+        self.base_url = self._env_or_raise("KITCHENOWL_API_URL")
+        self.household_id = household_id
+        token = self._env_or_raise("KITCHENOWL_API_KEY")
+        self.DEFAULT_HEADERS = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+
+    def _env_or_raise(self, name):
+        result = os.getenv(name)
+        if not result:
+            raise Exception(f"Please set the {name} environment variable.")
+        return result
+
+    def shopping_lists(self):
+        url = f"{self.base_url}/household/{self.household_id}/shoppinglist"
+        response = requests.get(url, headers=self.DEFAULT_HEADERS)
+        response.raise_for_status()
+        return response.json()
+
+    @cached_property
+    def list_id(self):
+        if selected_id := os.getenv("KITCHENOWL_SHOPPING_LIST_ID"):
+            return selected_id
+        else:
+            return self.shopping_lists()[0]["id"]
+
+    def list_items(self):
+        slist = self.shopping_lists()[0]
+        return [i["name"] for i in slist["items"]]
+
+    def add_item(self, item_name):
+        url = f"{self.base_url}/shoppinglist/{self.list_id}/add-item-by-name"
+        data = {"name": item_name}
+        response = requests.post(url, json=data, headers=self.DEFAULT_HEADERS)
+        response.raise_for_status()
+        return response.json()
+
+    def remove_item(self, item_name):
+        item_ids = [
+            item["id"]
+            for item in self.shopping_lists()[0]["items"]
+            if item["name"] == item_name
+        ]
+
+        responses = []
+        for item_id in item_ids:
+            url = f"{self.base_url}/shoppinglist/{self.list_id}/item"
+            response = requests.delete(
+                url, json={"item_id": item_id}, headers=self.DEFAULT_HEADERS
+            )
+            responses.append(response.json())
+        return responses
